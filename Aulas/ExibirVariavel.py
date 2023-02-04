187fcecf73463df4f1c169eb641abc6e41739dbe
VitorLt <vitor_leitee@hotmail.com>
2023-02-04 13:40:36 -0400

    commit de exercicios

---
+++ Aulas/ExibirVariavel.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o código do funcionario "))
+nome = input("Digite o nome do funcionário ")
+salario = float(input("Informe o salario "))
+ativo = True
+
+print("Código: %d "% codigo)
+print("Nome: %s "% nome)
+print("Salário: %.2f " % salario)
+print("Ativo: %r " % ativo)
